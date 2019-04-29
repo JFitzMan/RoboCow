9eaeebc43cf84d3e9d7cbc3d2728dd2d39acb018
JFitzMan <fitzpatrickj1@email.arizona.edu>
2019-04-28 21:06:30 -0700

    Added help

--- RoboCow.py
+++ RoboCow.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import os
+import asyncio
 
 TOKEN = ''
 with open(r"token.no") as file:
@@ -11,6 +12,8 @@ with open(r"ip.no") as file:
     IP = file.read()
 print(IP)
 
+GAME_CHANNELS = ['572156522071719945']
+
 eightBallResponses = []
 with open(r"eightBallResponses") as file:
     for line in file.readlines():
@@ -19,19 +22,52 @@ print(eightBallResponses)
 
 client = discord.Client()
 
+def getHelp():
+    return """List of commands:
+!ip - get server IP
+!server - get server status
+!roll xDx - roll dice
+!ask - ask magic 8 ball
+"""
+
+async def serverWatchdog(client):
+    ping = False
+    await client.wait_until_ready()
+    while True:
+        response = os.system("ping -c 1 " + IP)
+        if response == 0:
+            if ping == False:
+                ping = True
+                for chan in GAME_CHANNELS:
+                    channel = discord.Object(id=chan)
+                    print(channel)
+                    await client.send_message(channel, "Server is up")
+            else:
+                print('Ping unchanged: on')
+        else:
+            if ping == 1:
+                ping = False
+                channel = discord.Object(id='572156522071719945')
+                print(channel)
+                print('its off')
+            else:
+                print('Ping unchanged: off')
+
+        await asyncio.sleep(3)
+
 def isDad(message):
     if str(message.author) == 'cowsareinme#1533':
         return ', Dad'
     else: return ''
 
-async def rollDice(string):
+def rollDice(string):
     s = string.split()
     msg = ''
     print(s)
     nums = s[1].split('d')
     if len(nums) == 1: nums = s[1].split('D')
     if len(nums) == 1: 
-        await channel.send("Bad read format should look like '!roll xdx'")
+        msg = ("Bad read format should look like '!roll xdx'")
     else:
         dice = int(nums[1])
         times = int(nums[0])   
@@ -77,15 +113,19 @@ async def on_message(message):
     if message.content.startswith('!roll'):
         channel = message.channel
         content = str(message.content)
-        msg = diceRoll(content)
+        msg = rollDice(content)
         await channel.send(msg)
+    if message.content.startswith('!help'):
+        channel = message.channel
+        await channel.send(getHelp())
 
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('--------')
+  #client.loop.create_task(serverWatchdog(client))
+    print('LETS GO BOIS')
 
 client.run(TOKEN)
 
